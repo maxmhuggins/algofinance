d9b8fe46e50c47d91f583e76856c9da596670d1f
Max Huggins <max.huggins98@gmail.com>
2021-03-09 12:29:20 -0600

    Added normalized variations function

--- algofinance/generalanalysis/variations.py
+++ algofinance/generalanalysis/variations.py
@@ -12,6 +12,8 @@ gather data for analysis
 """
 
 import numpy as np
+import datareader as dr
+import matplotlib.pyplot as plt
 
 
 class Variations:
@@ -20,6 +22,8 @@ class Variations:
         self.Dates = dates
         self.Closes = closes
         self.Normalized = normalized
+
+        self.Dates, self.Variations = self.main()
 
     def absolute_variations(self):
         variations = []
@@ -32,28 +36,67 @@ class Variations:
             else:
                 diff = closes[i] - closes[i+1]
                 variations.append(diff)
-        return variations
 
-    def normal_variations(self):
-        normal_dates, normal_closes = self.normalizer()
-# RUN NORMALIZED VARIATIONS ALGO
+        return dates, variations
 
-    def normalizer(self):
-        normal_dates = []
-        normal_closes = []
+    def shifter(self):
+        shifted_dates = []
+        shifted_closes = []
 
         for i in range(0, len(self.Dates)):
-            normal_closes.append(self.Closes[i]-self.Closes[0])
-            normal_dates.append(self.Dates[i]-self.Dates[0])
+            shifted_closes.append(self.Closes[i]-self.Closes[0])
+            shifted_dates.append(self.Dates[i]-self.Dates[0])
 
-        return normal_dates, normal_closes
+        return shifted_dates, shifted_closes
+
+    def normalizer(self):
+        shifted_dates, shifted_closes = self.shifter()
+
+        normal_closes = []
+
+        for i in range(0, len(shifted_dates)):
+            new_normal_value = shifted_closes[i] / self.Closes[i]
+            normal_closes.append(new_normal_value)
+
+        return shifted_dates, normal_closes
+
+    def normalized_variations(self):
+        normal_dates, normal_closes = self.normalizer()
+
+        normal_variations = []
+
+        for i in range(0, len(normal_dates)):
+            if i == len(normal_dates)-1:
+                normal_variations.append(np.nan)
+            else:
+                variation = (normal_closes[i] - normal_closes[i+1]) \
+                    / normal_closes[i]
+                normal_variations.append(variation)
+
+        return normal_dates, normal_variations
 
     def main(self):
 
         if self.Normalized is None:
-            self.Variations = self.absolute_variations()
+            return self.absolute_variations()
 
         else:
-            self.Variations = self.normal_variations()
-            # Need way to grab the from zero closes for plotting, but don't
-            # want to have to run the function twice to get them
+            return self.normalized_variations()
+
+
+start = '2021-02-02'
+end = '2021-03-05'
+dates = (start, end)
+BTC = dr.DataReader('BTCUSDT', 'binance', dates, '1d')
+
+dates, closes = BTC.Dates, BTC.Closes
+
+BTCVariations = Variations(dates, closes, normalized=True)
+
+# plt.plot(BTCVariations.Dates, BTCVariations.Closes)
+print(BTCVariations.Closes)
+
+"""
+Need way to grab the from shifted lists for plotting, but don't
+want to have to run the function twice to get them
+"""
