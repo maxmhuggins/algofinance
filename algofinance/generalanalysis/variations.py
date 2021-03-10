0dfb097c8b9546594718d3e34a2d359c5f8012c1
Max Huggins <max.huggins98@gmail.com>
2021-03-09 18:29:28 -0600

    Resolved div by zero issue

--- algofinance/generalanalysis/variations.py
+++ algofinance/generalanalysis/variations.py
@@ -14,6 +14,8 @@ gather data for analysis
 import numpy as np
 import datareader as dr
 import matplotlib.pyplot as plt
+plt.style.use('classic')
+
 
 
 class Variations:
@@ -22,6 +24,7 @@ class Variations:
         self.Dates = dates
         self.Closes = closes
         self.Normalized = normalized
+        self.ShiftedDates, self.ShiftedCloses = self.shifter()
 
         self.Dates, self.Variations = self.main()
 
@@ -40,40 +43,42 @@ class Variations:
         return dates, variations
 
     def shifter(self):
-        shifted_dates = []
-        shifted_closes = []
+        shifted_dates, shifted_closes = [], []
 
         for i in range(0, len(self.Dates)):
             shifted_closes.append(self.Closes[i]-self.Closes[0])
             shifted_dates.append(self.Dates[i]-self.Dates[0])
 
         return shifted_dates, shifted_closes
-
-    def normalizer(self):
-        shifted_dates, shifted_closes = self.shifter()
-
-        normal_closes = []
-
-        for i in range(0, len(shifted_dates)):
-            new_normal_value = shifted_closes[i] / self.Closes[i]
-            normal_closes.append(new_normal_value)
-
-        return shifted_dates, normal_closes
-
+    """
+    this needs some work, but I think I can use it to also display shifted,
+    gain values from a start date
+    
+        def normalizer(self):
+            shifted_dates, shifted_closes = self.shifter()
+    
+            normal_closes = []
+    
+            for i in range(0, len(shifted_dates)):
+                new_normal_value = closes[i] / self.Closes[i]
+                normal_closes.append(new_normal_value)
+    
+            return shifted_dates, normal_closes
+    """
     def normalized_variations(self):
-        normal_dates, normal_closes = self.normalizer()
+        dates, closes = self.Dates, self.Closes
 
         normal_variations = []
 
-        for i in range(0, len(normal_dates)):
-            if i == len(normal_dates)-1:
+        for i in range(0, len(dates)):
+            if i == len(dates)-1:
                 normal_variations.append(np.nan)
             else:
-                variation = (normal_closes[i] - normal_closes[i+1]) \
-                    / normal_closes[i]
+                variation = (closes[i] - closes[i+1])\
+                    / closes[i]
                 normal_variations.append(variation)
 
-        return normal_dates, normal_variations
+        return dates, normal_variations
 
     def main(self):
 
@@ -87,15 +92,19 @@ class Variations:
 start = '2021-02-02'
 end = '2021-03-05'
 dates = (start, end)
-BTC = dr.DataReader('BTCUSDT', 'binance', dates, '1d')
+BTC = dr.DataReader('TSLA', 'yahoo', dates, '1d')
 
 dates, closes = BTC.Dates, BTC.Closes
 
-BTCVariations = Variations(dates, closes, normalized=True)
+BTCVariations = Variations(dates, closes)
 
-# plt.bar(BTCVariations.Dates, BTCVariations.Closes)
-print(BTCVariations.Closes)
-print(BTCVariations.Dates)
+plt.bar(BTCVariations.Dates, BTCVariations.Variations, color = 'blue')
+plt.xlim(BTCVariations.Dates[0],BTCVariations.Dates[-1])
+plt.show()
+
+plt.plot(BTCVariations.ShiftedDates, BTCVariations.ShiftedCloses)
+# print(BTCVariations.Variations)
+# print(BTCVariations.Dates)
 
 """
 Need way to grab the from shifted lists for plotting, but don't
