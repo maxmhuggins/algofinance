c3fe60a6a2fe6b6289d174ca800d985bbd7cfcc7
Max Huggins <max.huggins98@gmail.com>
2021-04-09 12:02:35 -0500

    Made a psuedo implementation of the average thing

--- algofinance/generalanalysis/crypto_averages.py
+++ algofinance/generalanalysis/crypto_averages.py
@@ -11,3 +11,28 @@ closes and then it will also take an average of all of the crypto markets and
 plot that w.r.t. time to visualize the health of the market. Maybe it should
 also look at variations, not sure yet.
 """
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import datareader as dr
+
+coins = []
+
+columns = np.loadtxt('./data/list_of_cryptos', delimiter=',', skiprows=1,
+                     dtype='str')
+
+for column in columns:
+    coins.append(column[0])
+
+start = '2021-01-02'
+end = '2021-03-05'
+dates = (start, end)
+BTC = dr.DataReader('BTCUSDT', 'binance', dates, tick='1d')
+
+for coin in coins:
+    current_coin = dr.DataReader('{}USDT'.format(coin), 'binance', dates,
+                                 tick='1d')
+    plt.plot(current_coin.Dates, current_coin.Closes, label=coin)
+    plt.legend(loc='best')
+    plt.show()
